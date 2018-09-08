43c7ecd650c7c7fdf476a9ffabfdfdbcda32575e
simol80r <simola.nayak@gmail.com>
2018-09-08 05:49:12 -0400

    Added facial expression assessment script
    uses openface, pls add your lines

---
+++ openface-lib/assessor.py
@@ -0,0 +1,31 @@
+import time
+
+start = time.time()
+
+import argparse
+import cv2
+import os
+import pickle
+import sys
+
+from operator import itemgetter
+
+import numpy as np
+np.set_printoptions(precision=2)
+import pandas as pd
+
+import openface
+
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+from sklearn.grid_search import GridSearchCV
+from sklearn.mixture import GMM
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+
+def compareExpression(img1, img2):
+    score = 0
+    #landmarks? normalized.
+    return score
